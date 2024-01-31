60722d953693b225ebc1cecaf8143797c3b4eafe
Dominik Przybyl <dominik.przybyl@wundermanthompson.com>
2024-02-01 00:57:26 +0100

    cleanup

--- examples/nodejs/index.ts
+++
@@ -1,7 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as asd from "@pulumi/asd";
-
-const myRandomResource = new asd.Random("myRandomResource", {length: 24});
-export const output = {
-    value: myRandomResource.result,
-};
--- examples/python/__main__.py
+++
@@ -1,7 +0,0 @@
-import pulumi
-import pulumi_xyz as asd
-
-my_random_resource = asd.Random("myRandomResource", length=24)
-pulumi.export("output", {
-    "value": my_random_resource.result,
-})
